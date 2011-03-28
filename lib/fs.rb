6ce13a332eb1787b16708f33b912f3fec3aaca4c
blindgaenger <blindgaenger@gmail.com>
2011-03-28 23:47:40 +0200

    moved module to base file

--- lib/fs.rb
+++ lib/fs.rb
@@ -1,81 +1,9 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__)) unless $LOAD_PATH.include?(File.dirname(__FILE__))
+
 require 'fileutils'
+require 'fs/base'
 
 module FS
   extend self
-
-  # FileUtils.touch
-  def touch(files)
-    FileUtils.touch(files)
-  end
-
-  # FileUtils#mkdir
-  def makedir(dirs)
-    FileUtils.mkdir(dirs)
-  end
-
-  # FileUtils#mkdir_p
-  def makedirs(dirs)
-    FileUtils.mkdir_p(dirs)
-  end
-  
-  # Dir#entries
-  def list(dir, pattern='*')
-    glob(dir, pattern)
-  end
-  
-  # Find#find
-  def find(dir, pattern='*')
-    glob(dir, '**', pattern)
-  end
-  
-  # FileUtils#mv
-  def move(*froms, to)
-    froms.each do |from|
-      FileUtils.mv(from, to)
-    end
-  end
-
-  # FileUtils#rm
-  def remove(*pathes)
-    FileUtils.rm(pathes, :verbose => true)
-  end
-
-  # File#open(file, 'w')
-  def write(file, content=nil, &block)
-    if block_given?
-      File.open(file, 'w', &block)
-    else
-      File.open(file, 'w') {|f| f.write(content) }
-    end
-  end
-  
-  # File#open(file, 'r')
-  def read(file, &block)
-    if block_given?
-      File.open(file, 'r', &block)
-    else
-      content = nil
-      File.open(file, 'r') {|f| content = f.read }
-      content
-    end
-  end
-
-  # always returns '/'
-  def root
-    '/'
-  end
-  
-  # Dir#home
-  def home(user=nil)
-    Dir.home(user)
-  end
-
-  private
-  
-  def glob(dir, *patterns)
-    Dir.glob(File.join(dir, patterns)).map do |path|
-      path.gsub(/^\.?#{dir}\/?/, '')
-    end
-  end
-  
+  extend FS::Base
 end
