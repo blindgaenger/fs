5b58a8fbcf50bf881b7f95306aa1463d2711b993
Manuel Korfmann <manu@korfmann.info>
2011-03-30 16:30:06 +0200

    nicer output 2

--- lib/fs/base.rb
+++ lib/fs/base.rb
@@ -112,13 +112,6 @@ module FS
       Dir.pwd
     end
 
-    # same as the `tree` shell command
-    def tree(dir='.')
-      output = []
-      visit_tree(output, '.', '', '', '', dir)
-      output.join("\n")
-    end
-
     private
     
     def assert_dir(path)
@@ -131,26 +124,5 @@ module FS
         path.gsub(/^#{fulldir}\/?/, '')
       end.extend(FS::FileTreeWrapper)
     end
-    
-    def visit_tree(output, parent_path, indent, arm, tie, node)
-      output << "#{indent}#{arm}#{tie}#{node}"
-      
-      node_path = File.expand_path(node, parent_path)
-      return unless File.directory?(node_path) && File.readable?(node_path)
-      
-      subnodes = FS.list(node_path)
-      return if subnodes.empty?
-      
-      arms = Array.new(subnodes.length - 1, '|') << '`'
-      arm_to_indent = {
-        ''  => '',
-        '|' => '|   ',
-        '`' => '    '
-      }
-      subnodes.each_with_index do |subnode, index|
-        visit_tree(output, node_path, indent + arm_to_indent[arm], arms[index], '-- ', subnode)
-      end
-    end
-    
   end
 end
